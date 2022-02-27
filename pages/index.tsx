0f82948e58dff04a1369c4afbde0c1e9e370626a
Ilya Matvitsevsky <Ilya.Matvitsevsky@softeq.com>
2022-02-27 23:15:58 +0300

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+const Index = () => {
+  return (
+    <div>
+      Important page
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default Index;
