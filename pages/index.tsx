89c90d91284eeb6cf01cc568b837c1c3a5ecf650
Ilya Matvitsevsky <Ilya.Matvitsevsky@softeq.com>
2022-02-28 00:10:13 +0300

    create navbar and routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import MainLayout from '../layouts/MainLayout';
 
 const Index = () => {
   return (
-    <div>
-      Important page
-      <Button>Button</Button>
-    </div>
+    <>
+      <MainLayout>
+        <div className="center">
+          <h1>Welcome</h1>
+          <h3>Здесь собраны лучшие треки!</h3>
+        </div>
+      </MainLayout>
+
+      <style jsx>
+        {`
+          .center {
+            margin-top: 150px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+        `}
+      </style>
+    </>
   );
 };
 
